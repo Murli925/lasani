48e585e56d4458808cc7db16e89e9dc82c0f7f36
Murli925 <43351624+Murli925@users.noreply.github.com>
2026-01-14 22:30:02 +0530

    Add files via upload

---
+++ src/components/shared/Breadcrumb.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+export default function Contact() {
+  return (
+
+<div classname="page-banner-area">
+  <div classname="container-fluid">
+    <div classname="page-banner-inner">
+      <ul classname="list text-uppercase">
+        <li>
+          <a classname="text-black hover:text-[#717FF8]" href="/">Home</a>
+        </li>
+        <li>Portfolio</li>
+      </ul>
+    </div>
+  </div>
+</div>
+  );
+}
